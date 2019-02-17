e4884c68b0202ec3a91fe266da632f97f99e92f9
Maxim Nekrasov <kgtu@mail.ru>
2019-02-17 16:12:04 -0500

    Ссылка на блок-схемы

--- task1.py
+++ task1.py
@@ -1,4 +1,5 @@
 # 1. Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
+# Блок-схемы тут: https://drive.google.com/file/d/1RgRCoicYjOREaWbwNvwSh9nkf3rDpnuI/view?usp=sharing
 
 x = int(input('Введите трехзначное число: '))
 x = str(x)
