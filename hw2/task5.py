29cf326c9579b8d86262d90108e53e8a666b2be0
Maxim Nekrasov <kgtu@mail.ru>
2019-02-25 00:57:24 -0500

    task 8

--- hw2/task5.py
+++ hw2/task5.py
@@ -1,6 +1,6 @@
 counter = 0
 for i in range(32, 128):
-    print(f'{i:3} {chr(i)}  ')  # todo: no EOL
+    print(f'{i:3}-{chr(i)}  ')  # todo: no EOL
     counter += 1
     if counter >= 10:
         print('\n')
