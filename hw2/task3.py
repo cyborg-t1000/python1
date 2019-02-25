6f512c29872d3d57e08ee6c334cb9165d83dbd26
Maxim Nekrasov <kgtu@mail.ru>
2019-02-25 04:37:46 -0500

    todos done

--- hw2/task3.py
+++ hw2/task3.py
@@ -1,7 +1,7 @@
 n = int(input('Number: '))
 
 while True:
-    print(n % 10)  # todo: print without EOL
+    print(n % 10, end='')
     n = int(n / 10)
     if n == 0:
         break
--- hw2/task5.py
+++ hw2/task5.py
@@ -1,7 +1,7 @@
 counter = 0
 for i in range(32, 128):
-    print(f'{i:3}-{chr(i)}  ')  # todo: no EOL
+    print(f'{i:3}-{chr(i)}  ', end='')
     counter += 1
     if counter >= 10:
-        print('\n')
+        print('')
         counter = 0
